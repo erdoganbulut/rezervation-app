7a599b15be53ab43cdbe726caedbd20dfdf00b33
Erdogan Bulut <ebulut@protel.com.tr>
2018-04-14 22:30:21 +0300

    Mode material.module to modules/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import {MaterialModule} from './modules/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 
