a5c63b7cd45fc8e5ab6380fa0da15544f8b80ef3
Erdogan Bulut <ebulut@protel.com.tr>
2018-04-19 15:17:37 +0300

    Fix sidenav, integrate to store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { OPEN, CLOSE, TRIGGER } from './store/sidenav/index';
+import { AppState } from './models/appstate.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  isDrawer$: Observable<AppState['sidenav']>;
+
+  constructor(private store: Store<AppState>) {
+    this.isDrawer$ = store.pipe(select('sidenav'));
+  }
+
+  open() {
+    this.store.dispatch({ type: OPEN });
+  }
+
+  close() {
+    this.store.dispatch({ type: CLOSE });
+  }
+
+  trigger() {
+    this.store.dispatch({ type: TRIGGER });
+  }
+
+  ngOnInit() {
+  }
+
 }
--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { OPEN, CLOSE, TRIGGER } from '../../store/sidenav/index';
+import { AppState } from '../../models/appstate.model';
 
 @Component({
   selector: 'app-topbar',
@@ -7,7 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
+
+  handleClickOpenSidenav() {
+    this.store.dispatch({ type: OPEN });
+  }
+
+  handleClickCloseSidenav() {
+    this.store.dispatch({ type: CLOSE });
+  }
+
+  handleClickTriggerSidenav() {
+    this.store.dispatch({ type: TRIGGER });
+  }
 
   ngOnInit() {
   }
